b6b21e2ac93ce7d3a207bf6e15b71c6a4419bcf5
Aaron Jenner <ajenner@guidewire.com>
2024-12-11 11:12:08 +0000

    Day 11 solution simplified

--- src/days/Day11.java
+++ src/days/Day11.java
@@ -2,21 +2,51 @@ package days;
 
 import templates.DayTemplate;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Day11 extends DayTemplate {
 
-    public static Long part1(ArrayList<String> inputs) {
-        return -1L;
+    Map<Long, Long> stones = new HashMap<>();
+
+    private void buildInitialStones(ArrayList<String> inputs) {
+        stones = new HashMap<>();
+        for (String stone : inputs.get(0).split(" ")) {
+            stones.put(Long.parseLong(stone), 1L);
+        }
     }
 
-    public static Long part2(ArrayList<String> inputs) {
-        return -1L;
+    private List<Long> advanceState(Long stone) {
+        long numDigits = (long) (Math.log10(stone) + 1);
+        if (stone == 0L) {
+            return List.of(1L);
+        }
+        if (numDigits % 2 == 0) {
+            var divisor = Math.pow(10, numDigits / 2);
+            long left = (long) (stone / divisor);
+            long right = (long) (stone % divisor);
+            return List.of(left, right);
+        }
+        return List.of(stone * 2024);
+    }
+
+    private Long dynamicSteps(int steps) {
+        List<Long> nextStones;
+        for (int i = 0; i < steps; i++) {
+            Map<Long, Long> nextCount = new HashMap<>();
+            for (var stone : stones.entrySet()) {
+                nextStones = advanceState(stone.getKey());
+                for (Long nextStone : nextStones) {
+                    nextCount.put(nextStone, nextCount.getOrDefault(nextStone, 0L) + stone.getValue());
+                }
+            }
+            stones = nextCount;
+        }
+        return stones.values().stream().mapToLong(l -> l).sum();
     }
 
     @Override
     public Object solve(boolean part1, ArrayList<String> inputs) {
-
-        return (part1)? part1(inputs).toString() : part2(inputs).toString();
+        buildInitialStones(inputs);
+        return (part1) ? dynamicSteps(25).toString() : dynamicSteps(75).toString();
     }
 }
