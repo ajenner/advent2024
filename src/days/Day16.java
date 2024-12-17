db72c1275c98ef81f63f52e96ef3c1b48e104641
ajenner <ajenner@tcd.ie>
2024-12-17 16:01:08 +0000

    Day 17 Solution

--- src/days/Day16.java
+++ src/days/Day16.java
@@ -1,22 +1,97 @@
 package days;
 
+import common.Point;
 import templates.DayTemplate;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Day16 extends DayTemplate {
 
-    public static Long part1(ArrayList<String> inputs) {
-        return -1L;
-    }
+    public int dijkstra(ArrayList<String> inputs, boolean part1) {
+        var queue = new PriorityQueue<Entry>();
+        var visited = new HashSet<CityBlock>();
+        int endX = inputs.getFirst().length() - 1;
+        int endY = inputs.size() - 1;
 
-    public static Long part2(ArrayList<String> inputs) {
-        return -1L;
+        CityBlock rightStart = new CityBlock(new Point(1, 0), 1, Point.Direction.RIGHT);
+        CityBlock downStart = new CityBlock(new Point(0, 1), 1, Point.Direction.DOWN);
+        queue.add(new Entry(rightStart, Character.getNumericValue(inputs.get(rightStart.p().y()).charAt(rightStart.p().x()))));
+        queue.add(new Entry(downStart, Character.getNumericValue(inputs.get(downStart.p().y()).charAt(downStart.p().x()))));
+
+        while (!queue.isEmpty()) {
+            var current = queue.poll();
+            if (visited.contains(current.node)) {
+                continue;
+            }
+            visited.add(current.node);
+            if (current.node.p().x() == endX && current.node.p().y() == endY) {
+                return current.heatLoss;
+            }
+            queue.addAll(current.getNeighbours(inputs));
+        }
+        return 0;
     }
 
     @Override
     public Object solve(boolean part1, ArrayList<String> inputs) {
+        return String.valueOf(dijkstra(inputs, part1));
+    }
+}
 
-        return (part1)? part1(inputs).toString() : part2(inputs).toString();
+record CityBlock(Point p, int cost, Point.Direction direction) {}
+
+class Entry implements Comparable<Entry> {
+
+    public CityBlock node;
+    public int heatLoss;
+
+    public Entry(CityBlock node, int heatLoss) {
+        this.node = node;
+        this.heatLoss = heatLoss;
+    }
+
+    public List<Entry> getNeighbours(ArrayList<String> inputs) {
+        List<Entry> neighbours = new ArrayList<>();
+        var left = getNextEntry(inputs, node.p().rotateDirectionLeft90Degrees(node.direction()));
+        if (left != null) {
+            neighbours.add(left);
+        }
+
+        var right = getNextEntry(inputs, node.p().rotateDirectionRight90Degrees(node.direction()));
+        if (right != null) {
+            neighbours.add(right);
+        }
+
+        var straight = getNextEntry(inputs, node.direction());
+        if (straight != null) {
+            neighbours.add(straight);
+        }
+
+        return neighbours;
+    }
+
+
+    private Entry getNextEntry(ArrayList<String> inputs, Point.Direction nextDirection) {
+        var nextCityBlock = new CityBlock(node.p().moveDirection(nextDirection), node.direction() == nextDirection ? node.cost() + 1 : 1, nextDirection);
+        if (nextCityBlock.p().inBounds(inputs.getFirst().length(), inputs.size())) {
+            return new Entry(nextCityBlock, this.heatLoss + Character.getNumericValue(inputs.get(nextCityBlock.p().y()).charAt(nextCityBlock.p().x())));
+        }
+        return null;
+    }
+
+    @Override
+    public int compareTo(Entry o) {
+        if (this.heatLoss != o.heatLoss) {
+            return Integer.compare(this.heatLoss, o.heatLoss);
+        } else if (this.node.direction() == o.node.direction() && this.node.cost() != o.node.cost()) {
+            return Integer.compare(this.node.cost(), o.node.cost());
+        } else if (this.node.p().y() != o.node.p().y()) {
+            return Integer.compare(this.node.p().y(), o.node.p().y());
+        } else {
+            return Integer.compare(this.node.p().x(), o.node.p().x());
+        }
     }
 }
--- src/days/Day17.java
+++ src/days/Day17.java
@@ -3,20 +3,166 @@ package days;
 import templates.DayTemplate;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day17 extends DayTemplate {
 
-    public static Long part1(ArrayList<String> inputs) {
-        return -1L;
+    public String part1(ArrayList<String> inputs) {
+        var registerA = Integer.parseInt(inputs.get(0).split("[^\\d-]+")[1]);
+        var registerB = Integer.parseInt(inputs.get(1).split("[^\\d-]+")[1]);
+        var registerC = Integer.parseInt(inputs.get(2).split("[^\\d-]+")[1]);
+        var operations = Arrays.stream(inputs.get(4).split("[^\\d-]+")).filter(s -> !s.isEmpty()).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        var computer = new Computer(registerA, registerB, registerC, operations);
+        computer.runComputer();
+        return computer.returnOutput();
     }
 
-    public static Long part2(ArrayList<String> inputs) {
-        return -1L;
+
+    public Long part2(ArrayList<String> inputs) {
+        var initialB = Integer.parseInt(inputs.get(1).split("[^\\d-]+")[1]);
+        var initialC = Integer.parseInt(inputs.get(2).split("[^\\d-]+")[1]);
+        var operations = Arrays.stream(inputs.get(4).split("[^\\d-]+")).filter(s -> !s.isEmpty()).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        List<Integer> program = new ArrayList<>();
+        List<Integer> remainingOperations = new ArrayList<>(operations);
+        long a = 0L;
+        while (!remainingOperations.isEmpty()) {
+            program.addFirst(remainingOperations.removeLast());
+            var programString = String.join(",", program.stream().map(Long::toString).toList());
+            Computer computer;
+            a--;
+            do {
+                computer = new Computer(++a, initialB, initialC, operations);
+                computer.runComputer(true, program);
+            } while (!computer.returnOutput().equals(programString));
+            if (!remainingOperations.isEmpty()) {
+                a = a << 3;
+            }
+        }
+        return a;
     }
 
     @Override
     public Object solve(boolean part1, ArrayList<String> inputs) {
-
-        return (part1)? part1(inputs).toString() : part2(inputs).toString();
+        return (part1) ? part1(inputs) : part2(inputs).toString();
     }
+
+    private static class Computer {
+        long registerA = 0;
+        long registerB = 0;
+        long registerC = 0;
+
+        List<Integer> operations;
+        List<Integer> output = new ArrayList<>();
+
+        public Computer(long a, long b, long c, List<Integer> operations) {
+            registerA = a;
+            registerB = b;
+            registerC = c;
+            this.operations = operations;
+        }
+
+        public void runComputer() {
+            runComputer(false, List.of());
+        }
+
+        /**
+         * 2,4, B = A % 8
+         * 1,1, B = B ^ 1
+         * 7,5, C = A >> B
+         * 1,5, B = B ^ 5
+         * 4,3, B = B ^ C
+         * 5,5, print ( B % 8 )
+         * 0,3, A = A >> 3
+         * 3,0  if (A != 0) restart
+         */
+        public void runComputer(boolean stopEarly, List<Integer> expectedOutput) {
+            boolean halted = false;
+            int currentPointer = 0;
+            long comboOperand;
+            while (!halted) {
+                int instruction = operations.get(currentPointer);
+                boolean jumped = false;
+                switch (instruction) {
+                    case 0:
+                        comboOperand = comboOperandValue(operations.get(currentPointer + 1));
+                        registerA = (long) (registerA / (Math.pow(2, comboOperand)));
+                        break;
+                    case 1:
+                        registerB = registerB ^ operations.get(currentPointer + 1);
+                        break;
+                    case 2:
+                        registerB = comboOperandValue(operations.get(currentPointer + 1)) % 8;
+                        break;
+                    case 3:
+                        if (registerA != 0) {
+                            currentPointer = operations.get(currentPointer + 1);
+                            jumped = true;
+                        }
+                        break;
+                    case 4:
+                        registerB = registerB ^ registerC;
+                        break;
+                    case 5:
+                        output.add((int) (comboOperandValue(operations.get(currentPointer + 1)) % 8));
+                        if (stopEarly && !outputValid(expectedOutput)) {
+                            return;
+                        }
+                        break;
+                    case 6:
+                        comboOperand = comboOperandValue(operations.get(currentPointer + 1));
+                        registerB = (long) (registerA / (Math.pow(2, comboOperand)));
+                        break;
+                    case 7:
+                        comboOperand = comboOperandValue(operations.get(currentPointer + 1));
+                        registerC = (long) (registerA / (Math.pow(2, comboOperand)));
+                        break;
+                    default:
+                        System.out.println("INVALID!!!!");
+                }
+                if (!jumped) {
+                    currentPointer += 2;
+                }
+                if (currentPointer >= operations.size() - 1) {
+                    halted = true;
+                }
+            }
+        }
+
+        private long comboOperandValue(int comboOperand) {
+            if (comboOperand >= 0 && comboOperand <= 3) {
+                return comboOperand;
+            }
+            if (comboOperand == 4) {
+                return registerA;
+            }
+            if (comboOperand == 5) {
+                return registerB;
+            }
+            if (comboOperand == 6) {
+                return registerC;
+            }
+            System.out.println("INVALID!!!!");
+            return -1;
+        }
+
+        public boolean outputValid(List<Integer> target) {
+            if (output.size() > target.size()) {
+                return false;
+            }
+            for (int i = 0; i < output.size(); i++) {
+                if (!output.get(i).equals(target.get(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public String returnOutput() {
+            return String.join(",", output.stream().map(Long::toString).toList());
+        }
+
+    }
+
 }
